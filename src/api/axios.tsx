8d564264d817f320e6088b5fce19bf600fbebd8f
Muahmmed Sahal <162565944+sahalnp@users.noreply.github.com>
2025-10-11 13:27:35 +0530

    changed host

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL =
   window.location.hostname === "localhost"
     ? "http://localhost:3000/api" 
-    : "https://kpi-tracking-back.onrender.com/api";
+    : "https://kpi-tracking-back.vercel.app/api";
 
 export const axiosInstance = axios.create({
   baseURL,
