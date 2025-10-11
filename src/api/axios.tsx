c266e4776a5a83dbfc98973a13b92cba71572a3d
Muahmmed Sahal <162565944+sahalnp@users.noreply.github.com>
2025-10-11 15:16:17 +0530

    change url

--- src/api/axios.tsx
+++ src/api/axios.tsx
@@ -3,7 +3,7 @@ import axios from "axios";
 const baseURL =
   window.location.hostname === "localhost"
     ? "http://localhost:3000/api" 
-    : "https://kpi-tracking-back-6domk2bqt-muhammed-sahals-projects-e6fd404c.vercel.app/api";
+    : "https://kpi-tracking-back.vercel.app/api";
 
 export const axiosInstance = axios.create({
   baseURL,
